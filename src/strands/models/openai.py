480fe3cc5d4ed037d242eea2bec5db376f053d8b
Patrick Gray <pgrayy@amazon.com>
2025-05-21 14:10:09 +0000

    openai - catch usage payload

--- src/strands/models/openai.py
+++ src/strands/models/openai.py
@@ -113,6 +113,8 @@ class OpenAIModel(SAOpenAIModel):
 
         yield {"chunk_type": "message_stop", "data": choice.finish_reason}
 
-        event = next(response)
-        if hasattr(event, "usage"):
-            yield {"chunk_type": "metadata", "data": event.usage}
+        # Skip remaining events as we don't have use for anything except the final usage payload
+        for event in response:
+            _ = event
+
+        yield {"chunk_type": "metadata", "data": event.usage}
