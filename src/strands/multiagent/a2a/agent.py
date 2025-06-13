ec33aa0f37af7e78b68203e0394bc3929991d341
jer <jbill7696@gmail.com>
2025-06-13 18:17:18 +0000

    wip: cleanup and wire up sync agent

--- src/strands/multiagent/a2a/agent.py
+++ src/strands/multiagent/a2a/agent.py
@@ -5,21 +5,20 @@ allowing it to be used in A2A-compatible systems.
 """
 
 import logging
+from typing import Any, Literal
 
-import httpx
 import uvicorn
-from a2a.server.apps import A2AStarletteApplication
+from a2a.server.apps import A2AFastAPIApplication, A2AStarletteApplication
 from a2a.server.request_handlers import DefaultRequestHandler
 from a2a.server.tasks import InMemoryTaskStore
-from a2a.types import (
-    AgentCard,
-    AgentSkill,
-)
+from a2a.types import AgentCapabilities, AgentCard, AgentSkill
+from fastapi import FastAPI
+from starlette.applications import Starlette
 
 from ...agent.agent import Agent as SAAgent
 from .executor import StrandsA2AExecutor
 
-logger = logging.getLogger(__name__)
+log = logging.getLogger(__name__)
 
 
 class A2AAgent:
@@ -31,36 +30,76 @@ class A2AAgent:
 
     def __init__(
         self,
-        agent: SAAgent | None,
-        agent_card: AgentCard | None,
-        skills: list[AgentSkill] | None,
-        version: str = "1.0.0",
+        agent: SAAgent,
+        *,
+        # AgentCard
+        name: str,
+        description: str,
         host: str = "localhost",
         port: int = 9000,
+        version: str = "0.0.1",
     ):
         """Initialize an A2A-compatible agent from a Strands agent.
 
         TODO: add args
 
         """
+        self.name = name
+        self.description = description
         self.host = host
         self.port = port
-        self.agent = agent
-        self.agent_card = agent_card
-        self.skills = skills
-        self.executor = StrandsA2AExecutor(self.agent)
-        self.httpx_client = httpx.AsyncClient()
+        self.http_url = f"http://{self.host}:{self.port}/"
+        self.version = version
+        self.strands_agent = agent
+        self.capabilities = AgentCapabilities()
         self.request_handler = DefaultRequestHandler(
-            agent_executor=self.executor,
+            agent_executor=StrandsA2AExecutor(self.strands_agent),
             task_store=InMemoryTaskStore(),
-            # push_notifier=a2a_tasks.InMemoryPushNotifier(self.httpx_client),
         )
 
-    def to_starlette(self):
-        """TODO."""
-        starlette_server = A2AStarletteApplication(agent_card=self.agent_card, http_handler=self.request_handler)
-        return starlette_server.build()
+    @property
+    def public_agent_card(self) -> AgentCard:
+        """AgentCard."""
+        return AgentCard(
+            name=self.name,
+            description=self.description,
+            url=self.http_url,
+            version=self.version,
+            skills=self.agent_skills,
+            defaultInputModes=["text"],
+            defaultOutputModes=["text"],
+            capabilities=self.capabilities,
+        )
 
-    def serve(self):
-        """TODO."""
-        uvicorn.run(self.to_starlette(), host=self.host, port=self.port)
+    @property
+    def agent_skills(self) -> list[AgentSkill]:
+        """AgentSkills."""
+        return []
+
+    def to_starlette_app(self) -> Starlette:
+        """Startlette app."""
+        starlette_app = A2AStarletteApplication(agent_card=self.public_agent_card, http_handler=self.request_handler)
+        return starlette_app.build()
+
+    def to_fastapi_app(self) -> FastAPI:
+        """FastAPI app."""
+        fastapi_app = A2AFastAPIApplication(agent_card=self.public_agent_card, http_handler=self.request_handler)
+        return fastapi_app.build()
+
+    def serve(self, app_type: Literal["fastapi", "starlette"] = "starlette", **kwargs: Any) -> None:
+        """Start the A2A server with the specified application type.
+
+        Args:
+            app_type: The type of application to serve, either "fastapi" or "starlette".
+            **kwargs: Additional keyword arguments to pass to uvicorn.run.
+        """
+        try:
+            log.info("Starting Strands agent A2A server...")
+            if app_type == "fastapi":
+                uvicorn.run(self.to_fastapi_app(), host=self.host, port=self.port, **kwargs)
+            else:
+                uvicorn.run(self.to_starlette_app(), host=self.host, port=self.port, **kwargs)
+        except KeyboardInterrupt:
+            log.warning("Server shutdown requested (KeyboardInterrupt).")
+        finally:
+            log.info("Strands agent A2A server has shutdown.")
--- src/strands/multiagent/a2a/executor.py
+++ src/strands/multiagent/a2a/executor.py
@@ -1,25 +1,24 @@
 """A2A Module."""
 
+import logging
+
 from a2a.server.agent_execution import AgentExecutor, RequestContext
 from a2a.server.events import EventQueue
+from a2a.types import UnsupportedOperationError
 from a2a.utils import new_agent_text_message
+from a2a.utils.errors import ServerError
 
-from ...agent.agent import Agent as SAAAgent
+from ...agent.agent import Agent as SAAgent
+from ...agent.agent_result import AgentResult as SAAgentResult
 
-
-class HelloWorldAgent:
-    """Hello World Agent."""
-
-    async def invoke(self) -> str:
-        """Hello World Agent."""
-        return "Hello World"
+log = logging.getLogger(__name__)
 
 
 class StrandsA2AExecutor(AgentExecutor):
-    """Test AgentProxy Implementation."""
+    """StrandsA2AExecutor."""
 
-    def __init__(self, agent: SAAAgent):
-        """Hello World Agent."""
+    def __init__(self, agent: SAAgent):
+        """StrandsA2AExecutor constructor."""
         self.agent = agent
 
     async def execute(
@@ -27,10 +26,13 @@ class StrandsA2AExecutor(AgentExecutor):
         context: RequestContext,
         event_queue: EventQueue,
     ) -> None:
-        """Hello World Agent."""
-        result = self.agent("tell me something about love")
-        await event_queue.enqueue_event(new_agent_text_message(result))
+        """Execute."""
+        result: SAAgentResult = self.agent(context.get_user_input())
+        if result.message and "content" in result.message:
+            for content_block in result.message["content"]:
+                if "text" in content_block:
+                    await event_queue.enqueue_event(new_agent_text_message(content_block["text"]))
 
     async def cancel(self, context: RequestContext, event_queue: EventQueue) -> None:
-        """Hello World Agent."""
-        raise Exception("cancel not supported")
+        """Cancel."""
+        raise ServerError(error=UnsupportedOperationError())
