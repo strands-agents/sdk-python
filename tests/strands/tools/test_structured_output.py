422bc25285e95241a91ad16dc054185fb382cd53
laithalsaadoon <alsaadoonlaith@gmail.com>
2025-06-13 17:36:25 -0500

    fix: inline basemodel classes

--- tests/strands/tools/test_structured_output.py
+++ tests/strands/tools/test_structured_output.py
@@ -1,57 +1,20 @@
-from typing import Literal
+from typing import Literal, Optional
 
-import pytest
 from pydantic import BaseModel, Field
 
 from strands.tools.structured_output import convert_pydantic_to_tool_spec
 from strands.types.tools import ToolSpec
 
 
-class User(BaseModel):
-    """A user of the system."""
+def test_convert_pydantic_to_tool_spec_basic():
+    class User(BaseModel):
+        """A user of the system."""
 
-    name: str = Field(description="The name of the user")
-    age: int = Field(description="The age of the user")
-    email: str = Field(description="The email of the user", default="")
+        name: str = Field(description="The name of the user")
+        age: int = Field(description="The age of the user")
+        email: str = Field(description="The email of the user", default="")
 
-
-class Employment(BaseModel):
-    """An employment of the user."""
-
-    company: str = Field(description="The company of the user")
-    title: Literal[
-        "CEO",
-        "CTO",
-        "CFO",
-        "CMO",
-        "COO",
-        "VP",
-        "Director",
-        "Manager",
-        "Other",
-    ] = Field(description="The title of the user", default="Other")
-
-
-# for a nested, more complex test
-class UserWithEmployment(User):
-    """A user of the system with employment."""
-
-    employment: Employment = Field(description="The employment of the user")
-
-
-@pytest.fixture
-def user_model():
-    return User
-
-
-@pytest.fixture
-def user_with_employment_model():
-    return UserWithEmployment
-
-
-@pytest.fixture
-def basic_user_tool_spec():
-    return {
+    basic_user_tool_spec = {
         "name": "User",
         "description": "A user of the system.",
         "inputSchema": {
@@ -73,11 +36,44 @@ def basic_user_tool_spec():
             }
         },
     }
+    tool_spec = convert_pydantic_to_tool_spec(User)
+
+    assert tool_spec == basic_user_tool_spec
+    assert ToolSpec(**tool_spec) == ToolSpec(**basic_user_tool_spec)
 
 
-@pytest.fixture
-def complex_user_tool_spec_json():
-    return {
+def test_convert_pydantic_to_tool_spec_complex():
+    class User(BaseModel):
+        """A user of the system."""
+
+        name: str = Field(description="The name of the user")
+        age: int = Field(description="The age of the user")
+        email: str = Field(description="The email of the user", default="")
+
+    class Employment(BaseModel):
+        """An employment of the user."""
+
+        company: str = Field(description="The company of the user")
+        title: Literal[
+            "CEO",
+            "CTO",
+            "CFO",
+            "CMO",
+            "COO",
+            "VP",
+            "Director",
+            "Manager",
+            "Other",
+        ] = Field(description="The title of the user", default="Other")
+
+    class UserWithEmployment(User):
+        """A user of the system with employment."""
+
+        employment: Employment = Field(description="The employment of the user")
+        employment_2: Optional[Employment] = Field(description="A part time employment of the user")
+
+    tool_spec = convert_pydantic_to_tool_spec(UserWithEmployment)
+    complex_user_tool_spec_json = {
         "name": "UserWithEmployment",
         "description": "A user of the system with employment.",
         "inputSchema": {
@@ -107,6 +103,21 @@ def complex_user_tool_spec_json():
                         },
                         "required": ["company"],
                     },
+                    "employment_2": {
+                        "type": ["object", "null"],
+                        "description": "A part time employment of the user",
+                        "properties": {
+                            "company": {"description": "The company of the user", "title": "Company", "type": "string"},
+                            "title": {
+                                "default": "Other",
+                                "description": "The title of the user",
+                                "enum": ["CEO", "CTO", "CFO", "CMO", "COO", "VP", "Director", "Manager", "Other"],
+                                "title": "Title",
+                                "type": "string",
+                            },
+                        },
+                        "required": ["company"],
+                    },
                 },
                 "title": "UserWithEmployment",
                 "description": "A user of the system with employment.",
@@ -114,23 +125,6 @@ def complex_user_tool_spec_json():
             }
         },
     }
-
-
-def test_convert_pydantic_to_tool_spec_basic(
-    user_model,
-    basic_user_tool_spec,
-):
-    tool_spec = convert_pydantic_to_tool_spec(user_model)
-
-    assert tool_spec == basic_user_tool_spec
-    assert ToolSpec(**tool_spec) == ToolSpec(**basic_user_tool_spec)
-
-
-def test_convert_pydantic_to_tool_spec_complex(
-    user_with_employment_model,
-    complex_user_tool_spec_json,
-):
-    tool_spec = convert_pydantic_to_tool_spec(user_with_employment_model)
 
     assert tool_spec == complex_user_tool_spec_json
     assert ToolSpec(**tool_spec) == ToolSpec(**complex_user_tool_spec_json)
