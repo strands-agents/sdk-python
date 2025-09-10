ba2f0b5de8955117724100e2f68b2a795c06baec
Aaron Farntrog <afarn@amazon.com>
2025-09-10 14:03:19 -0400

    Use case 1) Customers may have overriden the `DEFAULT_BEDROCK_MODEL_ID`, to some custom `model_id` thinking it'll be used automatically in the Bedrock init. Therefore, we should not modify/format it if it was changed.
    Use case 2) Customers are used to `DEFAULT_BEDROCK_MODEL_ID` being set to a valid model.
    And it's possibly they are therefore using
    It in their code to do things like dynamically set the model: `agent.model.set_config(model_id=DEFAULT_BEDROCK_MODEL_ID)`
    We therefore need to handle that assumption and keep the `DEFAULT_BEDROCK_MODEL_ID`
    as is: `"us.anthropic.claude-sonnet-4-20250514-v1:0"`

--- src/strands/models/bedrock.py
+++ src/strands/models/bedrock.py
@@ -31,7 +31,7 @@ from .model import Model
 logger = logging.getLogger(__name__)
 
 # See: `BedrockModel._get_default_model_with_warning` for why we need both
-DEFAULT_BEDROCK_MODEL_ID = "{}.anthropic.claude-sonnet-4-20250514-v1:0"
+DEFAULT_BEDROCK_MODEL_ID = "us.anthropic.claude-sonnet-4-20250514-v1:0"
 _DEFAULT_BEDROCK_MODEL_ID = "{}.anthropic.claude-sonnet-4-20250514-v1:0"
 DEFAULT_BEDROCK_REGION = "us-west-2"
 
@@ -789,7 +789,7 @@ class BedrockModel(Model):
             region_name (str): region for bedrock model
             model_config (Optional[dict[str, Any]]): Model Config that caller passes in on init
         """
-        if DEFAULT_BEDROCK_MODEL_ID != _DEFAULT_BEDROCK_MODEL_ID:
+        if DEFAULT_BEDROCK_MODEL_ID != _DEFAULT_BEDROCK_MODEL_ID.format("us"):
             return DEFAULT_BEDROCK_MODEL_ID
 
         model_config = model_config or {}
@@ -805,7 +805,7 @@ class BedrockModel(Model):
             ================== WARNING ==================
 
                 This region {region_name} does not support
-                our default inference endpoint: {DEFAULT_BEDROCK_MODEL_ID.format(prefix)}.
+                our default inference endpoint: {_DEFAULT_BEDROCK_MODEL_ID.format(prefix)}.
                 Update the agent to pass in a 'model_id' like so:
                 ```
                 Agent(..., model='valid_model_id', ...)
@@ -817,4 +817,4 @@ class BedrockModel(Model):
                 stacklevel=2,
             )
 
-        return DEFAULT_BEDROCK_MODEL_ID.format(prefix_inference_map.get(prefix, prefix))
+        return _DEFAULT_BEDROCK_MODEL_ID.format(prefix_inference_map.get(prefix, prefix))
