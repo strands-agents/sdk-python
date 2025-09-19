65a4022fbabf36f02ef0c9332f416c7a416a33cd
Dean Schmigelski <dbschmigelski+github@gmail.com>
2025-09-19 12:47:02 -0400

    linting

--- src/strands/event_loop/streaming.py
+++ src/strands/event_loop/streaming.py
@@ -2,7 +2,7 @@
 
 import json
 import logging
-from typing import Any, AsyncGenerator, AsyncIterable, Optional, cast
+from typing import Any, AsyncGenerator, AsyncIterable, Optional
 
 from ..models.model import Model
 from ..types._events import (
