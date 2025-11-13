4786b2d048af5094177408ec933b4fc9b6f19b2f
Arron Bailiss <arron@amazon.com>
2025-11-13 09:57:33 -0500

    add integ test for a2a

---
+++ tests_integ/a2a/test_multiagent_a2a.py
@@ -0,0 +1,26 @@
+import atexit
+import os
+import subprocess
+import time
+
+from strands.agent.a2a_agent import A2AAgent
+
+# Start our A2A server
+server_path = os.path.join(os.path.dirname(__file__), "a2a_server.py")
+server = subprocess.Popen(["python", server_path])
+
+
+def cleanup():
+    server.terminate()
+
+
+atexit.register(cleanup)
+time.sleep(5)  # Wait for A2A server to start
+
+# Connect to our A2A server
+a2a_agent = A2AAgent(endpoint="http://localhost:9000")
+
+# Invoke our A2A server
+result = a2a_agent("Hello there!")
+
+# TODO: assertions on result
