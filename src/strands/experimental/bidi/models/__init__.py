83a19e2f10383577623919fcb834d04bb2f2f769
Patrick Gray <pgrayy@amazon.com>
2025-12-05 12:25:24 -0500

    build - bidi - isolate nova provider

--- src/strands/experimental/bidi/models/__init__.py
+++ src/strands/experimental/bidi/models/__init__.py
@@ -1,10 +1,8 @@
 """Bidirectional model interfaces and implementations."""
 
 from .model import BidiModel, BidiModelTimeoutError
-from .nova_sonic import BidiNovaSonicModel
 
 __all__ = [
     "BidiModel",
     "BidiModelTimeoutError",
-    "BidiNovaSonicModel",
 ]
