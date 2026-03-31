41d6fee76d8dc33af91fbb911874fde57c5afd5c
Kien Pham <kien@kienpham.com>
2026-03-30 22:11:17 -0700

    resolve conflict'

--- src/strands/models/bedrock.py
+++ src/strands/models/bedrock.py
@@ -516,7 +516,7 @@ class BedrockModel(Model):
         # https://docs.aws.amazon.com/bedrock/latest/APIReference/API_runtime_CachePointBlock.html
         if "cachePoint" in content:
             cache_point = content["cachePoint"]
-            result: dict[str, Any] = {"type": cache_point["type"]}
+            result = {"type": cache_point["type"]}
             if "ttl" in cache_point:
                 result["ttl"] = cache_point["ttl"]
             return {"cachePoint": result}
