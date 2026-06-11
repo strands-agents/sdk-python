4aaeab54330cb64f4c7a786fc2ccc2158e85e0b7
Liz <91279165+lizradway@users.noreply.github.com>
2026-06-11 15:14:27 -0400

    refactor(cedar): remove unnecessary type re-exports

--- strands-ts/src/vended-interventions/cedar/index.ts
+++ strands-ts/src/vended-interventions/cedar/index.ts
@@ -1,3 +1,2 @@
 export { CedarAuthorization } from './cedar.js'
 export type { CedarAuthorizationConfig, ToolDefinition } from './cedar.js'
-export type { TypeAndId, EntityJson, CedarValueJson } from '@cedar-policy/cedar-wasm/nodejs'
