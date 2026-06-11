opieter-aws/harness-sdk

feat: make injector a plugin

strands-ts/src/memory/__tests__/memory-manager.test.ts
@@ -9,6 +9,8 @@ import { logger } from '../../logging/logger.js'
 import { Message, TextBlock, ToolUseBlock, ToolResultBlock } from '../../types/messages.js'
 import type { MessageData } from '../../types/messages.js'
 import { InvokeModelStage } from '../../middleware/index.js'
+import type { InvokeModelContext } from '../../middleware/index.js'
+import { createMockAgent } from '../../__fixtures__/agent-helpers.js'
 
 function createMockStore(
   name: string,
@@ -618,14 +620,14 @@ describe('MemoryManager', () => {
     it('does not register injection middleware when injection is disabled', () => {
       const mm = new MemoryManager({ stores: [createMockStore('test')] })
       const addMiddleware = vi.fn()
-      mm.initAgent({ addMiddleware } as any)
+      mm.initAgent(createMockAgent({ extra: { addMiddleware } as never }))
       expect(addMiddleware).not.toHaveBeenCalled()
     })
 
     it('registers an InvokeModelStage input middleware when injection is enabled', () => {
       const mm = new MemoryManager({ stores: [createMockStore('test')], injection: true })
       const addMiddleware = vi.fn()
-      mm.initAgent({ addMiddleware } as any)
+      mm.initAgent(createMockAgent({ extra: { addMiddleware } as never }))
 
       expect(addMiddleware).toHaveBeenCalledTimes(1)
       expect(addMiddleware.mock.calls[0]![0]).toBe(InvokeModelStage.Input)
@@ -636,22 +638,21 @@ describe('MemoryManager', () => {
       const store = createMockStore('s', { entries: [{ content: 'dark mode preferred' }] })
       const mm = new MemoryManager({ stores: [store], injection: true })
       const addMiddleware = vi.fn()
-      mm.initAgent({ addMiddleware } as any)
+      const agent = createMockAgent({ extra: { addMiddleware } as never })
+      mm.initAgent(agent)
 
-      const handler = addMiddleware.mock.calls[0]![1] as (ctx: {
-        messages: Message[]
-      }) => Promise<{ messages: Message[] }>
+      const handler = addMiddleware.mock.calls[0]![1] as (ctx: InvokeModelContext) => Promise<InvokeModelContext>
       const messages = [
         new Message({ role: 'assistant', content: [new TextBlock('prior')] }),
         new Message({ role: 'user', content: [new TextBlock('what is my plan')] }),
       ]
-      const result = await handler({ messages })
+      const result = await handler({ messages, agent } as unknown as InvokeModelContext)
 
       const folded = result.messages[result.messages.length - 1]!
       expect((folded.content[0] as TextBlock).text).toBe(
         '<memory>\n<entry source="s">dark mode preferred</entry>\n</memory>'
       )
-      expect(store.search).toHaveBeenCalledWith('what is my plan', { maxSearchResults: 1 })
+      expect(store.search).toHaveBeenCalledWith('what is my plan', { maxSearchResults: 5 })
     })
   })
 
@@ -710,7 +711,7 @@ describe('MemoryManager', () => {
 
         await provide(mm, [assistant('prior step'), user('what is my plan')])
 
-        expect(store.search).toHaveBeenCalledWith('what is my plan', { maxSearchResults: 1 })
+        expect(store.search).toHaveBeenCalledWith('what is my plan', { maxSearchResults: 5 })
       })
 
       it('uses the most recent assistant text on an autonomous (tool-result) turn', async () => {
@@ -719,7 +720,7 @@ describe('MemoryManager', () => {
 
         await provide(mm, [user('task'), assistant('the previous step result'), toolResult()])
 
-        expect(store.search).toHaveBeenCalledWith('the previous step result', { maxSearchResults: 1 })
+        expect(store.search).toHaveBeenCalledWith('the previous step result', { maxSearchResults: 5 })
       })
 
       it('honors a custom query', async () => {
@@ -728,7 +729,7 @@ describe('MemoryManager', () => {
 
         await provide(mm, [assistant('prior'), user('ask')])
 
-        expect(store.search).toHaveBeenCalledWith('custom query', { maxSearchResults: 1 })
+        expect(store.search).toHaveBeenCalledWith('custom query', { maxSearchResults: 5 })
       })
 
       it('skips (returns undefined) when a custom query returns undefined', async () => {
@@ -778,7 +779,7 @@ describe('MemoryManager', () => {
         })
         const mm = new MemoryManager({
           stores: [store],
-          injection: { maxInjectedSearchResults: 2, format: (entries) => entries.map((e) => e.content).join(',') },
+          injection: { maxInjectedSearchResults: 2, format: ({ entries }) => entries.map((e) => e.content).join(',') },
         })
 
         const text = await provide(mm, [assistant('prior'), user('ask')])
@@ -806,11 +807,21 @@ describe('MemoryManager', () => {
         expect(text).toBe('<memory>\n<entry>no source</entry>\n</memory>')
       })
 
+      it('escapes XML in entry content and source so untrusted text cannot break the block', () => {
+        const mm = new MemoryManager({ stores: [createMockStore('s')], injection: true })
+        const text = (mm as unknown as { _defaultInjectionFormat(e: MemoryEntry[]): string })._defaultInjectionFormat([
+          { content: 'a < b & c > d </entry>', storeName: 'pre"f' },
+        ])
+        expect(text).toBe(
+          '<memory>\n<entry source="pre&quot;f">a &lt; b &amp; c &gt; d &lt;/entry&gt;</entry>\n</memory>'
+        )
+      })
+
       it('honors a custom format', async () => {
         const store = createMockStore('s', { entries: [{ content: 'A' }] })
         const mm = new MemoryManager({
           stores: [store],
-          injection: { format: (entries) => `[${entries.map((e) => e.content).join('|')}]` },
+          injection: { format: ({ entries }) => `[${entries.map((e) => e.content).join('|')}]` },
         })
 
         await expect(provide(mm, [assistant('prior'), user('ask')])).resolves.toBe('[A]')

strands-ts/src/vended-plugins/context-injector/__tests__/plugin.test.ts
@@ -1,0 +1,114 @@
+import { describe, it, expect, vi } from 'vitest'
+import { ContextInjector } from '../plugin.js'
+import { escapeXml } from '../index.js'
+import { InvokeModelStage } from '../../../middleware/index.js'
+import { Message, TextBlock } from '../../../types/messages.js'
+import type { InvokeModelContext } from '../../../middleware/index.js'
+import { createMockAgent } from '../../../__fixtures__/agent-helpers.js'
+
+const user = (text: string) => new Message({ role: 'user', content: [new TextBlock(text)] })
+const assistant = (text: string) => new Message({ role: 'assistant', content: [new TextBlock(text)] })
+
+// Builds a mock agent that captures addMiddleware registrations, with a real appState/cancelSignal.
+function makeAgent() {
+  const addMiddleware = vi.fn()
+  const agent = createMockAgent({ extra: { addMiddleware } as never })
+  return { agent, addMiddleware }
+}
+
+describe('ContextInjector', () => {
+  describe('plugin interface', () => {
+    it('defaults to the strands:context-injector name', () => {
+      expect(new ContextInjector({ provide: async () => 'x' }).name).toBe('strands:context-injector')
+    })
+
+    it('honors a custom name (so multiple injectors can be told apart)', () => {
+      expect(new ContextInjector({ name: 'now', provide: async () => 'x' }).name).toBe('now')
+    })
+
+    it('registers an InvokeModelStage input middleware on initAgent', () => {
+      const { agent, addMiddleware } = makeAgent()
+      new ContextInjector({ provide: async () => 'x' }).initAgent(agent)
+
+      expect(addMiddleware).toHaveBeenCalledTimes(1)
+      expect(addMiddleware.mock.calls[0]![0]).toBe(InvokeModelStage.Input)
+      expect(typeof addMiddleware.mock.calls[0]![1]).toBe('function')
+    })
+  })
+
+  describe('registered handler', () => {
+    // Runs the handler the plugin registered, with a context backed by the mock agent.
+    async function run(plugin: ContextInjector, messages: Message[]) {
+      const { agent, addMiddleware } = makeAgent()
+      plugin.initAgent(agent)
+      const handler = addMiddleware.mock.calls[0]![1] as (ctx: InvokeModelContext) => Promise<InvokeModelContext>
+      return handler({ messages, agent } as unknown as InvokeModelContext)
+    }
+
+    it('folds provide() text into the latest user message', async () => {
+      const result = await run(new ContextInjector({ provide: async () => 'INJECTED' }), [
+        assistant('prior'),
+        user('ask'),
+      ])
+      const target = result.messages[result.messages.length - 1]!
+      expect((target.content[0] as TextBlock).text).toBe('INJECTED')
+      expect((target.content[1] as TextBlock).text).toBe('ask')
+    })
+
+    it('skips on a non-user turn by default (userTurn trigger)', async () => {
+      const provide = vi.fn(async () => 'x')
+      const input = [user('ask'), assistant('reply')]
+      const result = await run(new ContextInjector({ provide }), input)
+      expect(provide).not.toHaveBeenCalled()
+      expect(result.messages).toBe(input)
+    })
+
+    it("'everyTurn' injects regardless of the latest role", async () => {
+      const result = await run(new ContextInjector({ trigger: 'everyTurn', provide: async () => 'INJECTED' }), [
+        user('ask'),
+        assistant('reply'),
+      ])
+      // No later user message than index 0, so the fold targets it.
+      expect((result.messages[0]!.content[0] as TextBlock).text).toBe('INJECTED')
+    })
+
+    it('exposes appState and the cancel signal to provide', async () => {
+      const { agent, addMiddleware } = makeAgent()
+      let sawSignal = false
+      let sawAppState = false
+      new ContextInjector({
+        provide: async (ctx) => {
+          sawSignal = ctx.signal === agent.cancelSignal
+          sawAppState = ctx.appState === agent.appState
+          return undefined
+        },
+      }).initAgent(agent)
+      const handler = addMiddleware.mock.calls[0]![1] as (ctx: InvokeModelContext) => Promise<InvokeModelContext>
+      await handler({ messages: [user('ask')], agent } as unknown as InvokeModelContext)
+
+      expect(sawSignal).toBe(true)
+      expect(sawAppState).toBe(true)
+    })
+
+    it('fails open (passes context through) when provide throws', async () => {
+      const result = await run(
+        new ContextInjector({
+          provide: async () => {
+            throw new Error('boom')
+          },
+        }),
+        [assistant('prior'), user('ask')]
+      )
+      // Unchanged: the single original user message, no injected block.
+      const target = result.messages[result.messages.length - 1]!
+      expect(target.content).toHaveLength(1)
+      expect((target.content[0] as TextBlock).text).toBe('ask')
+    })
+  })
+
+  describe('escapeXml helper', () => {
+    it('escapes &, <, > for safe element content', () => {
+      expect(escapeXml('a < b & c > d')).toBe('a &lt; b &amp; c &gt; d')
+    })
+  })
+})

strands-agents/sdk-python

strands-ts/src/memory/__tests__/memory-manager.test.ts
@@ -9,6 +9,8 @@ import { logger } from '../../logging/logger.js'
 import { Message, TextBlock, ToolUseBlock, ToolResultBlock } from '../../types/messages.js'
 import type { MessageData } from '../../types/messages.js'
 import { InvokeModelStage } from '../../middleware/index.js'
+import type { InvokeModelContext } from '../../middleware/index.js'
+import { createMockAgent } from '../../__fixtures__/agent-helpers.js'
 
 function createMockStore(
   name: string,
@@ -618,14 +620,14 @@ describe('MemoryManager', () => {
     it('does not register injection middleware when injection is disabled', () => {
       const mm = new MemoryManager({ stores: [createMockStore('test')] })
       const addMiddleware = vi.fn()
-      mm.initAgent({ addMiddleware } as any)
+      mm.initAgent(createMockAgent({ extra: { addMiddleware } as never }))
       expect(addMiddleware).not.toHaveBeenCalled()
     })
 
     it('registers an InvokeModelStage input middleware when injection is enabled', () => {
       const mm = new MemoryManager({ stores: [createMockStore('test')], injection: true })
       const addMiddleware = vi.fn()
-      mm.initAgent({ addMiddleware } as any)
+      mm.initAgent(createMockAgent({ extra: { addMiddleware } as never }))
 
       expect(addMiddleware).toHaveBeenCalledTimes(1)
       expect(addMiddleware.mock.calls[0]![0]).toBe(InvokeModelStage.Input)
@@ -636,22 +638,21 @@ describe('MemoryManager', () => {
       const store = createMockStore('s', { entries: [{ content: 'dark mode preferred' }] })
       const mm = new MemoryManager({ stores: [store], injection: true })
       const addMiddleware = vi.fn()
-      mm.initAgent({ addMiddleware } as any)
+      const agent = createMockAgent({ extra: { addMiddleware } as never })
+      mm.initAgent(agent)
 
-      const handler = addMiddleware.mock.calls[0]![1] as (ctx: {
-        messages: Message[]
-      }) => Promise<{ messages: Message[] }>
+      const handler = addMiddleware.mock.calls[0]![1] as (ctx: InvokeModelContext) => Promise<InvokeModelContext>
       const messages = [
         new Message({ role: 'assistant', content: [new TextBlock('prior')] }),
         new Message({ role: 'user', content: [new TextBlock('what is my plan')] }),
       ]
-      const result = await handler({ messages })
+      const result = await handler({ messages, agent } as unknown as InvokeModelContext)
 
       const folded = result.messages[result.messages.length - 1]!
       expect((folded.content[0] as TextBlock).text).toBe(
         '<memory>\n<entry source="s">dark mode preferred</entry>\n</memory>'
       )
-      expect(store.search).toHaveBeenCalledWith('what is my plan', { maxSearchResults: 1 })
+      expect(store.search).toHaveBeenCalledWith('what is my plan', { maxSearchResults: 5 })
     })
   })
 
@@ -698,7 +699,7 @@ describe('MemoryManager', () => {
       })
 
       it('passes an injection config object through unchanged', () => {
-        const cfg = { maxInjectedSearchResults: 5 }
+        const cfg = { maxEntries: 5 }
         expect(injectionConfig(new MemoryManager({ stores: [createMockStore('s')], injection: cfg }))).toBe(cfg)
       })
     })
@@ -710,7 +711,7 @@ describe('MemoryManager', () => {
 
         await provide(mm, [assistant('prior step'), user('what is my plan')])
 
-        expect(store.search).toHaveBeenCalledWith('what is my plan', { maxSearchResults: 1 })
+        expect(store.search).toHaveBeenCalledWith('what is my plan', { maxSearchResults: 5 })
       })
 
       it('uses the most recent assistant text on an autonomous (tool-result) turn', async () => {
@@ -719,7 +720,7 @@ describe('MemoryManager', () => {
 
         await provide(mm, [user('task'), assistant('the previous step result'), toolResult()])
 
-        expect(store.search).toHaveBeenCalledWith('the previous step result', { maxSearchResults: 1 })
+        expect(store.search).toHaveBeenCalledWith('the previous step result', { maxSearchResults: 5 })
       })
 
       it('honors a custom query', async () => {
@@ -728,7 +729,7 @@ describe('MemoryManager', () => {
 
         await provide(mm, [assistant('prior'), user('ask')])
 
-        expect(store.search).toHaveBeenCalledWith('custom query', { maxSearchResults: 1 })
+        expect(store.search).toHaveBeenCalledWith('custom query', { maxSearchResults: 5 })
       })
 
       it('skips (returns undefined) when a custom query returns undefined', async () => {
@@ -772,13 +773,13 @@ describe('MemoryManager', () => {
         expect(store.search).toHaveBeenCalled()
       })
 
-      it('honors maxInjectedSearchResults and caps the rendered entries', async () => {
+      it('honors maxEntries and caps the rendered entries', async () => {
         const store = createMockStore('s', {
           entries: [{ content: 'A' }, { content: 'B' }, { content: 'C' }],
         })
         const mm = new MemoryManager({
           stores: [store],
-          injection: { maxInjectedSearchResults: 2, format: (entries) => entries.map((e) => e.content).join(',') },
+          injection: { maxEntries: 2, format: ({ entries }) => entries.map((e) => e.content).join(',') },
         })
 
         const text = await provide(mm, [assistant('prior'), user('ask')])
@@ -806,11 +807,21 @@ describe('MemoryManager', () => {
         expect(text).toBe('<memory>\n<entry>no source</entry>\n</memory>')
       })
 
+      it('escapes XML in entry content and source so untrusted text cannot break the block', () => {
+        const mm = new MemoryManager({ stores: [createMockStore('s')], injection: true })
+        const text = (mm as unknown as { _defaultInjectionFormat(e: MemoryEntry[]): string })._defaultInjectionFormat([
+          { content: 'a < b & c > d </entry>', storeName: 'pre"f' },
+        ])
+        expect(text).toBe(
+          '<memory>\n<entry source="pre&quot;f">a &lt; b &amp; c &gt; d &lt;/entry&gt;</entry>\n</memory>'
+        )
+      })
+
       it('honors a custom format', async () => {
         const store = createMockStore('s', { entries: [{ content: 'A' }] })
         const mm = new MemoryManager({
           stores: [store],
-          injection: { format: (entries) => `[${entries.map((e) => e.content).join('|')}]` },
+          injection: { format: ({ entries }) => `[${entries.map((e) => e.content).join('|')}]` },
         })
 
         await expect(provide(mm, [assistant('prior'), user('ask')])).resolves.toBe('[A]')

strands-ts/src/vended-plugins/context-injector/__tests__/plugin.test.ts
@@ -1,0 +1,114 @@
+import { describe, it, expect, vi } from 'vitest'
+import { ContextInjector } from '../plugin.js'
+import { escapeXml } from '../index.js'
+import { InvokeModelStage } from '../../../middleware/index.js'
+import { Message, TextBlock } from '../../../types/messages.js'
+import type { InvokeModelContext } from '../../../middleware/index.js'
+import { createMockAgent } from '../../../__fixtures__/agent-helpers.js'
+
+const user = (text: string) => new Message({ role: 'user', content: [new TextBlock(text)] })
+const assistant = (text: string) => new Message({ role: 'assistant', content: [new TextBlock(text)] })
+
+// Builds a mock agent that captures addMiddleware registrations, with a real appState/cancelSignal.
+function makeAgent() {
+  const addMiddleware = vi.fn()
+  const agent = createMockAgent({ extra: { addMiddleware } as never })
+  return { agent, addMiddleware }
+}
+
+describe('ContextInjector', () => {
+  describe('plugin interface', () => {
+    it('defaults to the strands:context-injector name', () => {
+      expect(new ContextInjector({ renderContent: async () => 'x' }).name).toBe('strands:context-injector')
+    })
+
+    it('honors a custom name (so multiple injectors can be told apart)', () => {
+      expect(new ContextInjector({ name: 'now', renderContent: async () => 'x' }).name).toBe('now')
+    })
+
+    it('registers an InvokeModelStage input middleware on initAgent', () => {
+      const { agent, addMiddleware } = makeAgent()
+      new ContextInjector({ renderContent: async () => 'x' }).initAgent(agent)
+
+      expect(addMiddleware).toHaveBeenCalledTimes(1)
+      expect(addMiddleware.mock.calls[0]![0]).toBe(InvokeModelStage.Input)
+      expect(typeof addMiddleware.mock.calls[0]![1]).toBe('function')
+    })
+  })
+
+  describe('registered handler', () => {
+    // Runs the handler the plugin registered, with a context backed by the mock agent.
+    async function run(plugin: ContextInjector, messages: Message[]) {
+      const { agent, addMiddleware } = makeAgent()
+      plugin.initAgent(agent)
+      const handler = addMiddleware.mock.calls[0]![1] as (ctx: InvokeModelContext) => Promise<InvokeModelContext>
+      return handler({ messages, agent } as unknown as InvokeModelContext)
+    }
+
+    it('folds renderContent() text into the latest user message', async () => {
+      const result = await run(new ContextInjector({ renderContent: async () => 'INJECTED' }), [
+        assistant('prior'),
+        user('ask'),
+      ])
+      const target = result.messages[result.messages.length - 1]!
+      expect((target.content[0] as TextBlock).text).toBe('INJECTED')
+      expect((target.content[1] as TextBlock).text).toBe('ask')
+    })
+
+    it('skips on a non-user turn by default (userTurn trigger)', async () => {
+      const renderContent = vi.fn(async () => 'x')
+      const input = [user('ask'), assistant('reply')]
+      const result = await run(new ContextInjector({ renderContent }), input)
+      expect(renderContent).not.toHaveBeenCalled()
+      expect(result.messages).toBe(input)
+    })
+
+    it("'everyTurn' injects regardless of the latest role", async () => {
+      const result = await run(new ContextInjector({ trigger: 'everyTurn', renderContent: async () => 'INJECTED' }), [
+        user('ask'),
+        assistant('reply'),
+      ])
+      // No later user message than index 0, so the fold targets it.
+      expect((result.messages[0]!.content[0] as TextBlock).text).toBe('INJECTED')
+    })
+
+    it('exposes appState and the cancel signal to renderContent', async () => {
+      const { agent, addMiddleware } = makeAgent()
+      let sawSignal = false
+      let sawAppState = false
+      new ContextInjector({
+        renderContent: async (ctx) => {
+          sawSignal = ctx.signal === agent.cancelSignal
+          sawAppState = ctx.appState === agent.appState
+          return undefined
+        },
+      }).initAgent(agent)
+      const handler = addMiddleware.mock.calls[0]![1] as (ctx: InvokeModelContext) => Promise<InvokeModelContext>
+      await handler({ messages: [user('ask')], agent } as unknown as InvokeModelContext)
+
+      expect(sawSignal).toBe(true)
+      expect(sawAppState).toBe(true)
+    })
+
+    it('fails open (passes context through) when renderContent throws', async () => {
+      const result = await run(
+        new ContextInjector({
+          renderContent: async () => {
+            throw new Error('boom')
+          },
+        }),
+        [assistant('prior'), user('ask')]
+      )
+      // Unchanged: the single original user message, no injected block.
+      const target = result.messages[result.messages.length - 1]!
+      expect(target.content).toHaveLength(1)
+      expect((target.content[0] as TextBlock).text).toBe('ask')
+    })
+  })
+
+  describe('escapeXml helper', () => {
+    it('escapes &, <, > for safe element content', () => {
+      expect(escapeXml('a < b & c > d')).toBe('a &lt; b &amp; c &gt; d')
+    })
+  })
+})