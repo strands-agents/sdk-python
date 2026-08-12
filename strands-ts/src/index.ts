fb5d5a1740f792d12180b913a7c485014729ecd7
opieter-aws <opieter@amazon.com>
2026-08-12 13:57:51 -0400

    remove cachettl export

--- strands-ts/src/index.ts
+++ strands-ts/src/index.ts
@@ -183,7 +183,7 @@ export {
 } from './models/streaming.js'
 
 // Model provider types
-export type { BaseModelConfig, CountTokensOptions, StreamOptions, CacheConfig, CacheTTL } from './models/model.js'
+export type { BaseModelConfig, CountTokensOptions, StreamOptions, CacheConfig } from './models/model.js'
 
 export { Model } from './models/model.js'
 
