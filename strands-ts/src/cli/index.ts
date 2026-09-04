60c946c4a36ab13dc164db9edde0a2259007438b
Prasanna <77713617+DinoQuinten@users.noreply.github.com>
2026-09-04 23:16:18 +0530

    Revert "TEMP: endpoint support for testing (REVERT BEFORE PR)"
    This reverts commit b8a69fa694a0b0b4689931052d519fe48390bb60.

--- strands-ts/src/cli/index.ts
+++ strands-ts/src/cli/index.ts
@@ -2,14 +2,3 @@ export { createTheme, stripAnsi, type Theme, type ThemeOptions } from './theme.j
 export { renderMarkdown, type MarkdownOptions } from './markdown.js'
 export { Spinner } from './spinner.js'
 export { CliPrinter, type CliPrinterOptions } from './cli-printer.js'
-export {
-  createModelFromEnv,
-  createAgentFromEnv,
-  detectProvider,
-  resolveProviderName,
-  loadDotEnv,
-  parseEnvFile,
-  ProviderConfigError,
-  type ProviderName,
-  type ResolvedModel,
-} from './env.js'
